7d331df827cb42df9e222eff2ef4d87e396fe99b
Jennifer Clayton <stacked777@gmail.com>
2021-02-08 21:39:49 -0700

    9PMchange

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,21 @@
 // API current prices
 // https://api.coindesk.com/v1/bpi/currentprice.json
- 
+
 //list 20 most popular coins
 
 // create variables
 
 // functions
 
-// click event & console log api data 
-var x = document.createElement("BUTTON")
+// click event & console log api data
+var x = document.createElement("BUTTON");
 
-function getCryptoPrices (searchValue){
-fetch (
-    'https://api.coindesk.com/v1/bpi/currentprice.json')
-    .then (response => response.json())
-    .then (data => {
-        console.log(searchValue)
-    })
-
+function getCryptoPrices(searchValue) {
+  fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(searchValue);
+    });
 }
- getCryptoPrices("list prices")
+getCryptoPrices("list prices");
+$(".test-button").click(console.log("hello"));
