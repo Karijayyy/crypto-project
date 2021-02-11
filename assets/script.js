3c922d174b876f1ee2d3a0e2f8cd64d3d3842d54
Kari Johnson <kari@crownmediamarketing.com>
2021-02-11 07:46:11 -0700

    already did

--- assets/script.js
+++ assets/script.js
@@ -15,17 +15,49 @@ function getBitcoinPrice() {
       console.log(data);
       let getBitcoinPriceNow = data.bpi.USD.rate
       $("#bitcoin").text(getBitcoinPriceNow)
-     });
+    });
 }
 getBitcoinPrice();
 $(".test-button").click(console.log("hello bitcoin"));
 
 // function for all other crypto prices
+
+//  let getCryptoPriceNow = data.rates."PUT CORRELATING ABBREVIATION HERE"
+// $("#b").text(getCryptoPriceNow)
+
+// abbreviations for coins 
+
+// ethereum ---> etc 
+// xrp ----> xrp
+// stellar lumens ----> xlm
+// polkadot -----> dot
+// litecoin ---> ltc
+// dogecoin ----> doge
+// cosmos -----> atom 
+// vechain  -----> vet
+// cardano ----> ada 
+// dash -----> dash 
+// chainlink -----> link
+// balancer-----> bal
+// eos -----> eos 
+// solana -----> sol 
+// tron -----> trx
+// nano -----> nano
+// filecoin-----> fil
+// decred -----> dcr
+// digibyte -----> dgb
+// nem ---> xem 
+// neo -----> neo 
+// stellar -----> xlm 
+
+
 function getCryptoPrices() {
   fetch("http://api.coinlayer.com/api/live?access_key=8bab17ed8bc64ebebd3b1dbe4ad46a98")
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
+      let getCryptoPriceNow = data.rates.BCH
+      $("#bitcoinCash").text(getCryptoPriceNow)
     });
 }
 getCryptoPrices();
